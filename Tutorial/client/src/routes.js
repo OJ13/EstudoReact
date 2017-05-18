1546f2d081d64900d52d59c3bba192236770a4f9
Osmar Junior <Osmar Junior>
2017-05-18 19:01:27 -0300

    add aula youtube

--- Tutorial/client/src/routes.js
+++ Tutorial/client/src/routes.js
@@ -3,14 +3,14 @@ import { Router, Route, hasHistory, IndexRoute } from 'react-router';
 import { Home, Welcome, About, Contact } from './components';
 
 const routes = (
-    `<Router history = { hasHistory } >
+    <Router history = { hasHistory } >
         <Route path = "/" component = { Home } >
             <IndexRoute component = { Welcome } >
             <Route path = "/about" component = { About }/>
             <Route path = "/contact" component = { Contact }/>
             </IndexRoute>
         </Route> 
-    </Router >`
+    </Router >
 );
 
 export default routes;
